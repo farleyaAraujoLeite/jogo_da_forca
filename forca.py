1e5d764fd9c724d9ed8378dd5307f8089d9de800
farleyaAraujoLeite <farley2181@gmail.com>
2023-02-20 16:30:59 -0300

    ajustando algumas coisas.

--- forca.py
+++ forca.py
@@ -1,16 +1,17 @@
 from random import choice
 import emoji
 from playsound import playsound
+
             
 def forca():
-        
+  
     music_less_point = '/home/farley/Documentos/jogo_da_forca/songs/lees_point.mp3'
     music_looser = '/home/farley/Documentos/jogo_da_forca/songs/loose.mp3'
     music_winner = '/home/farley/Documentos/jogo_da_forca/songs/winner.mp3'
     #lista
     frutas = ['melao', 'banana', 'abacaxi', 'uva', 'laranja', 'melancia', 'abacate', 'goiaba']
     print('===================================================================================')
-    print(f'Tente adivinhar qual foi a fruta escolhida pelo sistema na lista a seguir em 3 tentativas:{frutas}')
+    print(f'Tente adivinhar qual foi a fruta escolhida pelo sistema na lista a seguir em 3 tentativas:\n{frutas}'.upper())
     print('===================================================================================')
     escolhida = choice(frutas)
     erros = 0
@@ -23,7 +24,7 @@ def forca():
             print('Agora não pode mais errar !')
         palpite = input('Tente novamente:')
         if palpite == escolhida:
-            print('Parabéns, você acertou e venceu!', emoji.emojize(':party_popper:'))
+            print('Parabéns, você acertou!', emoji.emojize(':party_popper:'))
             playsound(music_winner)
         elif erros == 3:
             print(f'Você errou {erros} vezes e foi para a forca.',emoji.emojize(':anxious_face_with_sweat:'))
